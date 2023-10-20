c3675f95e9dfd9b7ed0fad2258b63263d511e37f
Nepalcem <mark.emailtest@gmail.com>
2023-10-20 23:55:54 +0300

    Update db.js

--- utils/db.js
+++ utils/db.js
@@ -3,7 +3,11 @@ const config = require("../config");
 
 async function query(sql, params) {
   const connection = await mysql.createConnection(config.db);
-  const [results] = await connection.execute(sql, params);
+    const sanitizedParams = params.map((param) =>
+      param !== undefined ? param : null
+    );
+
+  const [results] = await connection.execute(sql, sanitizedParams);
 
   connection.end();
   return results;
